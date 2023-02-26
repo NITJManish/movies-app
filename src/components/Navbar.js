1a7bf09cc794d2d0122105fe1680c05d9cc091c5
neera <kumarmanish150799@gmail.com>
2023-02-27 03:15:06 +0530

    movie UI part

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+    render(){
+        return (
+            <div className='nav'>
+            <div className='search-container'></div>
+              <input />
+              <button id="search-btn">search</button>
+            </div>
+          )
+    }
+  
+}
+
+export default Navbar
